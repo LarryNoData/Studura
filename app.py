b405022e379565162d98eb4f74a7af7bb2d86bc7
Larry NoData <siddavies10@gmail.com>
2025-06-11 18:14:32 +0100

    Fixed port handling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -21,4 +22,6 @@ def profile(username):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
