6347e2a9e3e3551d9eed0b65420a9418a786e173
Larry NoData <siddavies10@gmail.com>
2025-06-30 08:20:00 +0100

    app.py changes for local url

--- app.py
+++ app.py
@@ -13,9 +13,6 @@ load_dotenv()
 
 app = Flask(__name__)
 
-load_dotenv()
-
-app = Flask(__name__)
 
 # Convert legacy Postgres URL if needed
 if os.environ.get("DATABASE_URL", "").startswith("postgres://"):
@@ -209,7 +206,9 @@ def logout():
     return redirect('/')
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    if os.environ.get("FLASK_ENV") == "development":
+        with app.app_context():
+            db.create_all()
+
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
