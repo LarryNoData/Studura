01d5c8a90ddeb1a44e4390850132745c5ef307c4
Larry NoData <siddavies10@gmail.com>
2025-07-03 22:38:25 +0100

    fixing bugs in launch.sh

--- launch.sh
+++ launch.sh
@@ -4,12 +4,13 @@ echo "Starting Studura at $(date)"
 set -e  # Exit immediately if any command fails
 
 # Run migration and capture errors
-if ! python manage.py db upgrade; then
-    echo "DB upgrade failed"
-    exit 1
+if flask db upgrade; then
+    echo "Migration complete"
 else
-    echo "Database upgraded successfully"
+    echo "Migration failed"
+    exit 1
 fi
+
 
 # Start the Gunicorn server
 exec gunicorn app:app
