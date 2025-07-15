30c324090b9a310909ac73fc32e6f2070abcd08f
Larry NoData <siddavies10@gmail.com>
2025-07-15 13:53:10 +0100

    more major changes to launch

--- launch.sh
+++ launch.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-echo "Starting Studura at $(date)"
+echo "Starting Studura at $(date -u)"
 
 set -e  # Exit immediately if any command fails
 
-
-python manage.py db stamp 3f9c2a1b7e4a
-
+# Stamp the database to the latest migration revision
+python manage.py db stamp 3f9c2a1b7e4a || {
+    echo "DB stamp failed"
+    exit 1
+}
 
 # Run migration and capture errors
 if ! python manage.py db upgrade; then
     echo "DB upgrade failed"
     exit 1
 else
-    echo "Database upgraded succesfully"
+    echo "Database upgraded successfully"
 fi
 
 # Start the Gunicorn server
